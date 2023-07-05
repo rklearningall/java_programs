49e74d954be07ecea13530e81353d06775bf79fc
venkatesh <venkateshpaladugu13@example.com>
2023-07-05 16:28:36 +0530

    day1

---
+++ src/DAY1/SumAndAverage.java
@@ -0,0 +1,22 @@
+package DAY1;
+
+import java.util.Scanner;
+
+public class SumAndAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double Sum = num1 +num2;
+        double Average = Sum /2;
+
+        System.out.println("The sum two numbers is "+Sum);
+        System.out.println("Average of two number is "+Average);
+
+    }
+
+}
---
+++ src/DAY1/Swapnumber.java
@@ -0,0 +1,31 @@
+package DAY1;
+
+import java.util.Scanner;
+
+public class Swapnumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("before swap number");
+        System.out.println("first number "+num1);
+        System.out.println("second number " +num2);
+
+
+        double temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("After swap number");
+        System.out.println("first number "+num1);
+        System.out.println("second number " +num2);
+
+        scanner.close();
+
+    }
+}
